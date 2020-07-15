f45596d19500d960a6f1dc6b6459318a561c68e7
Noah.Yoon <ahnomk@gmail.com>
2020-07-15 23:36:45 +0900

    first commit node react

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: 1
+    },
+    password: {
+        type: String,
+        minlength: 5
+    },
+    lastname: {
+        type: String,
+        maxlength: 50
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
+//다른곳에서 쓰기 위해서 엑스포트해줘야할 필요가 있다
